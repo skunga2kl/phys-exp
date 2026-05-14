e15230cea0a417d309a5e04e56f3ee23b5e361ba
smalltown <skunga2kl@gmail.com>
2026-05-14 14:33:19 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Silk.NET.Windowing;
+using Silk.NET.OpenGL;
+using Silk.NET.Maths;
+using System.Net;
+
+class Program
+{
+    static IWindow window;
+    static GL gl;
+
+    static void Main(string[] args)
+    {
+        var options = WindowOptions.Default;
+
+        options.Size = new Vector2D<int>(800, 600);
+        options.Title = "physics experiment";
+
+        window = Window.Create(options);
+
+        window.Load += OnLoad;
+        window.Update += OnUpdate;
+        window.Render += OnRender;
+    }
+
+    static void OnLoad()
+    {
+        gl = GL.GetApi(window);
+
+        gl.ClearColor(0.1f, 0.1f, 0.15f, 1f);
+    }
+
+    static void OnUpdate(double dt)
+    {
+        // empty for now
+    } 
+
+    static void OnRender(double dt)
+    {
+        gl.Clear(ClearBufferMask.ColorBufferBit);
+    }
+}
